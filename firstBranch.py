353023144cbac38ba41d06fdf071fe41b02cfe43
Neroget <ice279198@gmail.com>
2021-02-07 13:41:26 +0300

    modify firstBranch

--- firstBranch.py
+++ firstBranch.py
@@ -6,3 +6,4 @@ Created on Sun Feb  7 13:29:45 2021
 """
 
 print ('Hello from firstBranch')
+print ('Hello GitHub')
---
+++ modify firstBranch.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 13:39:37 2021
+
+@author: troik
+"""
+
+print ('this modification firstBranch')
